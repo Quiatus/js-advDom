54557e9d5c0b80f28bb56c1ca2a3886d4fd643fd
Quiatus <Quiatus@gmail.com>
2024-05-26 22:01:53 +0200

    add cookies banner

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header')
 
-const openModal = function () {
+const openModal = (e) => {
+  e.preventDefault()
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +20,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +30,19 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+
+message.style.backgroundColor = '#37383d'
+message.style.width = '100vw'
+message.style.padding = '0.5rem'
+
+header.append(message)
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => message.remove())
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px'
+
+//document.documentElement.style.setProperty('--color-primary', 'orange') // targets root elemen
